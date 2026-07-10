6d5314ee0e78b5b18f6e9905232a31e61c12226a
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2026-07-10 15:48:27 -0300

    Add test expressions and main with same ideas

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 import { LikehoodWeighting } from "./controllers/LikehoodWeighting.js";
+import { MetropolisHasting } from "./controllers/MetropolisHasting.js";
+import { SecuentialMonteCarlo } from "./controllers/SecuentialMonteCarlo.js";
+import { Normal } from "./language/distributions.js";
+import { PrimitiveValue } from "./language/primitives.js";
 import { parseOne } from "./parser/sexpr.js";
+import { observe_expression_1, sample_expresion_2, sample_observe_expression_1 } from "./testExpresion.js";
 
-let program = '42'
+let program = '(let [mu (sample (normal 0 1))] (observe (normal mu 1) 2.3) mu)'
 
 console.log("Program: ",program)
 
-const ast = parseOne(program);
-console.log("Parsed program: ", ast)
+let ats = parseOne(program)
 
-const controller = new LikehoodWeighting()
+const controller_lw = new LikehoodWeighting()
 
-const message = controller.run(program, Math.random)
+let message_lw = controller_lw.run(program, Math.random, [], 100000)
 
-console.log("Message:", message)
+console.log("LW Mean:",controller_lw.mean(message_lw[0] as Array<number>, message_lw[1] as Array<number>))
 
+const controller_smc = new SecuentialMonteCarlo()
+
+const message_smc = controller_smc.run(program, Math.random,  new Array(20000).fill(Math.random), 20000)
+
+console.log("SMC Mean:", controller_smc.mean(message_smc as Array<number>))
+
+const controller_mh = new MetropolisHasting()
+
+const message_mh = controller_mh.run(program, Math.random, [], 60000, 3000)
+
+console.log("MH Mean: ", controller_mh.mean(message_mh as Array<number>))
--- testExpresion.ts
+++ testExpresion.ts
@@ -18,7 +18,7 @@ export const sample_expresion_2 = '(let [x (sample (normal 0 1)) y (sample (norm
 
 export const observe_expression_1 = '(observe (normal 0 1) 2)'
 
-export const sample_observe_expression_1 = '(let [m (sample (normal 0 1))] (observe (normal m 1) 2.0) m)'
+export const sample_observe_expression_1 = '(let [mu (sample (normal 0 1))] (observe (normal mu 1) 2.3) mu)' // Mean 1.150
 export const sample_observe_expresion_2 = '(let [m (sample (normal 0 1))] (observe (normal m 1) 2) (observe (normal m 1) 3) m)' // Mean  1.6667
 export const sample_obnserve_expresion_3 = '(let [x (sample (normal 0 1)) y (sample (normal x 1))] (observe (normal y 1) 2) x)' // Mean 0.667
 
